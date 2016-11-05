ee91780446cdb4d7ef62922d599c53c7297ab698
95filip <maruszczakf@student.mini.pw.edu.pl>
2016-11-05 12:08:32 +0100

    Rozszerzenie bazy danych

---
+++ DatabaseAccess/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess
+{
+    class Doctor : User
+    {
+        public WorkingTime MondayWorkingTime { get; set; }
+        public WorkingTime TuesdayWorkingTime { get; set; }
+        public WorkingTime WednesdayWorkingTime { get; set; }
+        public WorkingTime ThursdayWorkingTime { get; set; }
+        public WorkingTime FridayWorkingTime { get; set; }
+        public WorkingTime SaturdayWorkingTime { get; set; }
+        public WorkingTime SundayWorkingTime { get; set; }
+    }
+}
---
+++ DatabaseAccess/WorkingTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAccess
+{
+    class WorkingTime : Entity
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public WorkingTime()
+        {
+
+        }
+    }
+}
