9d2402716d83c8e3d5aeef506c2d0e9da293f16c
Pienio <pienbartl@gmail.com>
2016-11-07 15:17:16 +0100

    Dodanie przeglądania wizyt, archiwalnych oraz aktualnych

---
+++ Visits/WizList.xaml.cs
@@ -0,0 +1,47 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Visits
+{
+    /// <summary>
+    /// Interaction logic for WizList.xaml
+    /// </summary>
+    public partial class WizList : Window
+    {
+        private IEnumerable wizytyakt; 
+        private IEnumerable wizytyarc; 
+        public WizList(IEnumerable wizyta, IEnumerable wizytar)
+        {
+            InitializeComponent();
+            wizytyakt = wizyta;
+            wizytyarc = wizytar;
+            Listawizyt.ItemsSource = wizytyakt;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox.Header = "Planowane";
+            Listawizyt.ItemsSource = wizytyakt;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            groupBox.Header = "Archiwalne";
+            Listawizyt.ItemsSource = wizytyarc;
+        }
+    }
+}
